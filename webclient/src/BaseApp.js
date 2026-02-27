8c956195e37893755f944fcd5d2d3c739a2cc5f1
Vitor Dantas <seuvitor@gmail.com>
2026-02-26 21:16:30 -0300

    Replace eslint for Biome

--- webclient/src/BaseApp.js
+++ webclient/src/BaseApp.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import { Toolbar } from '@mui/material';
-import { BaseAppScope, useBaseAppScopeState } from 'react-aws-cognito-lambda-dynamodb-base-prototype-app';
+import { Toolbar } from "@mui/material";
+import {
+	BaseAppScope,
+	useBaseAppScopeState,
+} from "react-aws-cognito-lambda-dynamodb-base-prototype-app";
 
-import AppBar from './AppBar';
-import MessageArea from './MessageArea';
-import SpinnerArea from './SpinnerArea';
+import AppBar from "./AppBar";
+import MessageArea from "./MessageArea";
+import SpinnerArea from "./SpinnerArea";
 
 const BaseApp = ({ appConfig, appRoutes }) => {
-  const { routes } = useBaseAppScopeState(appRoutes);
+	const { routes } = useBaseAppScopeState(appRoutes);
 
-  return <BaseAppScope appConfig={appConfig} routes={routes}>
-      <AppBar routes={routes} />
-      <Toolbar />
-      <MessageArea />
-      <SpinnerArea />
-    </BaseAppScope>;
+	return (
+		<BaseAppScope appConfig={appConfig} routes={routes}>
+			<AppBar routes={routes} />
+			<Toolbar />
+			<MessageArea />
+			<SpinnerArea />
+		</BaseAppScope>
+	);
 };
 
 export default BaseApp;
